a93aa1df4b75e69e6f5e1d293798d8959fc06569
Бауыржан <@->
2019-01-07 02:12:24 +0600

    Добавьте файлы проекта.

---
+++ C_Sharp_Homework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Homework
+{
+
+    class Program
+    {
+        private static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbers = new int[5];
+
+                for (int i = 0; i <= numbers.Length; i++)
+                {
+                    numbers[i] = counter++;
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
